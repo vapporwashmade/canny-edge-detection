02b13de3cde34fc50352ffbc94e10368031db602
vapporwashmade <vappor.washmade@gmail.com>
2026-06-27 19:17:31 -0700

    create sliders to adjust canny thresholds

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import cv2
-import matplotlib
 
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+def nothing(x):
+    pass
 
-# Loading grayscale image
-image_path = "pommel.jpg"
-img_gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("pommel.jpg", cv2.IMREAD_GRAYSCALE)
+img_blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-# Gaussian blur
-img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+cv2.namedWindow('Canny Tuner', cv2.WINDOW_NORMAL)
+cv2.createTrackbar('Low Thresh', 'Canny Tuner', 50, 255, nothing)
+cv2.createTrackbar('High Thresh', 'Canny Tuner', 150, 255, nothing)
 
-# Canny edge detection
-edges = cv2.Canny(img_blur, threshold1=150, threshold2=350)
+cv2.waitKey(100)
 
-cv2.imwrite("pommel_line_drawing.jpg", edges)
-# Plot Original Grayscale
-plt.subplot(1, 2, 1)
-plt.title("Original (Grayscale)")
-plt.imshow(img_gray, cmap='gray')
-plt.axis('off')
+while True:
+    low = cv2.getTrackbarPos('Low Thresh', 'Canny Tuner')
+    high = cv2.getTrackbarPos('High Thresh', 'Canny Tuner')
 
-# Plot Canny Edges
-plt.subplot(1, 2, 2)
-plt.title("Canny Edge Detection")
-plt.imshow(edges, cmap='gray')
-plt.axis('off')
+    if low == -1 or high == -1:
+        break
 
-plt.tight_layout()
-plt.show()
+    edges = cv2.Canny(img_blur, low, high)
+    cv2.imshow('Canny Tuner', edges)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
